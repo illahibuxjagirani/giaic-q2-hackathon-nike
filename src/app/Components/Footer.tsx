03623fd269c442707361cdf3b3fdfdf64225815f
Your Name <illahibuxjagirani@gmail.com>
2024-12-12 22:04:50 +0500

    first

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,81 +1,152 @@
 import Image from "next/image";
-import Link from "next/link";
 
-export default function Footer() {
+import email from "../../../public/email.png"
+import fb from "../../../public/fb.png"
+import twitter from "../../../public/twitter.png"
+import instagram from "../../../public/instagram.png"
+import github from "../../../public/github.png"
+import payment from "../../../public/payment.png"
+
+export default function Footer(){
   return (
-    <div className="px-4 md:px-[50px] bg-black text-white py-8 mt-4 space-y-16 text-sm">
-      
-      {/* Top Section */}
-      <div className="flex flex-col md:flex-row justify-between gap-8">
+    <div className="bg-[#F0F0F0]">
 
-        {/* Navigation Links */}
-        <div className="flex flex-wrap md:flex-nowrap w-full md:w-[75%] gap-8 md:gap-[10%]">
-          
-          {/* Column 1 */}
-          <div className="space-y-4">
-            <ul className="flex flex-col gap-3 uppercase">
-              <Link href={"/"}>Find a Store</Link>
-              <Link href={"/"}>Become a Member</Link>
-              <Link href={"/"}>Sign up for Email</Link>
-              <Link href={"/"}>Send Us Feedback</Link>
-              <Link href={"/"}>Student Discounts</Link>
-            </ul>
-          </div>
+{/* Updates div */}
 
-          {/* Column 2 */}
-          <div className="space-y-4">
-            <ul className="flex flex-col gap-3 text-[#7E7E7E]">
-              <Link href={"/"} className="uppercase text-white">Get Help</Link>
-              <Link href={"/"}>Order Status</Link>
-              <Link href={"/"}>Delivery</Link>
-              <Link href={"/"}>Returns</Link>
-              <Link href={"/"}>Payment Options</Link>
-              <Link href={"/"}>Contact Us On Nike.com Inquiries</Link>
-              <Link href={"/"}>Contact Us On All Other Inquiries</Link>
-            </ul>
-          </div>
 
-          {/* Column 3 */}
-          <div className="space-y-4">
-            <ul className="flex flex-col gap-3 text-[#7E7E7E]">
-              <Link href={"/"} className="uppercase text-white">About Nike</Link>
-              <Link href={"/"}>News</Link>
-              <Link href={"/"}>Career</Link>
-              <Link href={"/"}>Investors</Link>
-              <Link href={"/"}>Sustainability</Link>
-            </ul>
-          </div>
-        </div>
 
-        {/* Social Icons */}
-        <div className="flex gap-3 justify-center items-center md:justify-start">
-          <Image src={"/github.png"} alt="GitHub" width={30} height={30} />
-          <Image src={"/fb.png"} alt="Facebook" width={30} height={30} />
-          <Image src={"/yt.png"} alt="YouTube" width={30} height={30} />
-          <Image src={"/insta.png"} alt="Instagram" width={30} height={30} />
-        </div>
-      </div>
+{/* footer div */}
+<div className="px-10 md:px-16 flex md:flex-row flex-col justify-center items-center py-5 relative">
+  
 
-      {/* Bottom Section */}
-      <div className="flex flex-col md:flex-row justify-between items-center gap-4 text-[#7E7E7E]">
+{/* Latest offers */}
+<div className=" bg-black flex md:flex-row flex-col items-center justify-between mx-5 md:mx-16 py-[36px] md:px-[64px] px-5 gap-8 rounded-xl absolute lg:-top-[150px] md:-top-[150px] sm:-top-[250px] -top-[270px]">
+  
+  <div>
+<h1 className="text-white sm:text-[40px] text-[35px] font-extrabold lg:w-[65%] leading-[40px]">STAY UPTO DATE ABOUT OUR LATEST OFFERS</h1>
+  </div>
 
-        {/* Left Section */}
-        <div className="flex flex-col md:flex-row gap-4 items-center text-center md:text-left">
-          <div className="flex gap-3 items-center">
-            <Image src={"/location.png"} alt="Location" width={10} height={13} />
-            <p className="text-white">Pakistan</p>
-          </div>
-          <p>© 2023 Nike, Inc. All Rights Reserved</p>
-        </div>
+  <div className="lg:w-[350px] space-y-3 w-full">
 
-        {/* Right Section */}
-        <div className="flex flex-wrap justify-center md:justify-end gap-4">
-          <Link href={"/"}>Guides</Link>
-          <Link href={"/"}>Terms of Sale</Link>
-          <Link href={"/"}>Terms of Use</Link>
-          <Link href={"/"}>Nike Privacy Policy</Link>
-        </div>
-      </div>
+<div className="relative">
+  <Image src={email} alt="Email" className="absolute left-3 top-3"/>
+  <input type="text" name="" id="" className="w-full rounded-full py-3 pl-12" placeholder="Enter your Email address"/>
+</div>
+
+<button className="bg-white w-full rounded-full py-3 font-bold">Subscribe to Newsletter</button>
+
+  </div>
+
+ 
+
+</div>
+
+
+{/* last div */}
+<div className="w-full space-y-10">
+
+{/* left div */}
+<div className="flex md:flex-row gap-5 flex-col justify-between pt-20">
+
+<div className="md:w-[30%] space-y-3">
+  <h1 className="text-[34px] font-extrabold uppercase">shop.co</h1>
+
+  <p className="text-sm">We have clothes that suits your style and which you’re proud to wear. From women to men.</p>
+
+  <div className="flex py-3 gap-5">
+<Image src={twitter} alt="Twitter"/>
+<Image src={fb} alt="Facebook"/>
+<Image src={instagram} alt="Instagram"/>
+<Image src={github} alt="Github"/>
+  </div>
+</div>
+
+
+
+<div className="md:w-[70%] flex md:flex-row flex-col gap-10 justify-between">
+
+{/* 1st two div */}
+<div className="flex md:justify-around justify-between w-full">
+
+<div className="space-y-3">
+
+<h1 className="font-semibold tracking-[5px] uppercase">Company</h1>
+<ul className="space-y-2">
+<li>About</li>
+<li>Features</li>
+<li>Works</li>
+<li>Career</li>
+</ul>
+
+</div>
+
+<div className="space-y-3">
+
+<h1 className="font-semibold tracking-[5px] uppercase">help</h1>
+<ul className="space-y-2">
+<li>Customer Support</li>
+<li>Delivery Details</li>
+<li>Terms and Conditions</li>
+<li>Privacy Policy</li>
+</ul>
+
+</div>
+</div>
+
+
+{/* 2nd two div */}
+<div className="flex md:justify-around justify-between w-full">
+
+<div className="space-y-3">
+
+<h1 className="font-semibold tracking-[5px] uppercase">faq</h1>
+<ul className="space-y-2">
+<li>Account</li>
+<li>Manage Deliveries</li>
+<li>Orders</li>
+<li>Payments</li>
+</ul>
+
+</div>
+
+<div className="space-y-3">
+
+<h1 className="font-semibold tracking-[5px] uppercase">resources</h1>
+<ul className="space-y-2">
+<li>Free eBooks</li>
+<li>Development Tutorials</li>
+<li>How to - Blog</li>
+<li>YouTube Playlist</li>
+</ul>
+
+</div>
+</div>
+
+</div>
+</div>
+
+
+{/* bottom div */}
+<div className="flex justify-between py-5 border-t-2 border-slate-300">
+
+{/* left div */}
+<div>
+  <p className="text-[#00000099]">Shop.co © 2000-2023, All Rights Reserved</p>
+</div>
+
+
+{/* right div */}
+<div>
+<Image src={payment} alt=" Payment"/>
+</div>
+
+
+</div>
+</div>
+
+</div>
+
+
     </div>
-  );
+  )
 }
--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,94 +1,106 @@
+"use client";
 import Image from "next/image";
+import cross from "../../../public/cross.png";
+import cross2 from "../../../public/cross2.png";
+import downArrow from "../../../public/down-arrow.png";
+import search from "../../../public/search.png";
+import search1 from "../../../public/search1.png";
+import burger from "../../../public/burger.png";
+import account from "../../../public/account.png";
+import cart from "../../../public/cart.png";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setMenuOpen(!menuOpen);
+  }
+
+ 
+
   return (
-    <header className="w-full sticky top-0 z-40">
-
-      {/* 1st Header Div: Top Bar */}
-      <div className="px-4 md:px-[50px] bg-[#F5F5F5] flex justify-between h-[36px] items-center">
-
-        {/* Logo Section */}
-        <div>
-          <Link href={"/"}>
-            <Image src="/logo1.png" alt="Logo1" width={24} height={24} />
-          </Link>
+    <div className="sticky top-0 z-50">
+      {/* Top Div */}
+      <div className="px-10 md:px-16 h-[30px] bg-black flex w-full justify-between text-white items-center">
+        <div className="flex sm:gap-3 gap-1 justify-center w-full">
+          <p className="sm:text-sm text-xs">Sign up and get 20% off to your first order</p>
+          <button className="underline hover:text-slate-200 text-xs sm:text-sm">Sign Up Now</button>
         </div>
-
-        {/* Navigation Links */}
-        <div className="hidden md:flex">
-          <ul className="flex text-sm">
-            <Link href={"/"} className="px-4 border-r-2 border-slate-600">Find a Store</Link>
-            <Link href={"/"} className="px-4 border-r-2 border-slate-600">Help</Link>
-            <Link href={"/"} className="px-4 border-r-2 border-slate-600">Join Us</Link>
-            <Link href={"/"} className="px-4 border-slate-600">Sign In</Link>
-          </ul>
-        </div>
+        <Image src={cross} alt="Close" className="h-[20px] w-[20px] sm:scale-100 scale-75 cursor-pointer" />
       </div>
 
-      {/* 2nd Header Div: Main Navigation */}
-      <div className="px-4 md:px-[50px] h-[60px] flex items-center justify-between bg-white">
-
-        {/* Logo Section */}
-        <div>
-          <Link href={"/"}>
-            <Image src={"/logo2.png"} alt="Logo2" width={59} height={21} />
-          </Link>
-        </div>
-
-        {/* Main Links */}
-        <div className="hidden md:flex">
-          <ul className="flex gap-5">
-            <Link href={"/"}>Home</Link>
-            <Link href={"/products"}>Men</Link>
-            <Link href={"/products"}>Women</Link>
-            <Link href={"/products"}>Kids</Link>
-            <Link href={"/products"}>Sales</Link>
-            <Link href={"/products"}>Products</Link>
-          </ul>
-        </div>
-
-        {/* Search and Icons */}
-        <div className="flex items-center gap-3 w-full md:w-auto">
-          {/* Search Bar */}
-          <div className="flex items-center relative w-full md:w-auto">
+      {/* Bottom Div */}
+      <div className="h-[65px] px-10 sm:px-16 flex items-center bg-white">
+        <div className="h-[62px] flex justify-between items-center w-full">
+          {/* Logo Div */}
+          <div className="flex gap-3 items-center">
+            <div className="h-[20px] w-[20px] flex items-center justify-center">
             <Image
-              src={"/search.png"}
-              height={24}
-              width={24}
-              alt="Search"
-              className="absolute left-2 cursor-pointer"
+              onClick={toggleMenu}
+              src={menuOpen ? cross2: burger}
+              alt="Burger Menu"
+              className="sm:hidden block cursor-pointer "
+              />
+              </div>
+
+          
+            <Link href="/" className="font-extrabold text-2xl sm:text-3xl uppercase sm:hidden lg:block">
+              shop.co
+            </Link>
+          </div>
+
+          {/* Toggle Menu */}
+          <div
+            className={`absolute left-0 top-20 w-[50%] h-[85vh] bg-slate-200 rounded-r-lg shadow-lg shadow-white transition-all duration-1000 ease-in-out ${
+              menuOpen ? "translate-x-0" : "-translate-x-full"
+            }`}
+          >
+            <ul className="flex gap-10 flex-col items-center py-10 text-xl">
+              <Link href="/" className="flex gap-2 items-center">
+                <span>Shop</span> <Image src={downArrow} alt="Arrow" className="fill-white" />
+              </Link>
+              <Link href="/">On Sale</Link>
+              <Link href="/arrivals">New Arrivals</Link>
+              <Link href="/">Brands</Link>
+            </ul>
+          </div>
+
+          {/* Desktop Navigation */}
+          <div>
+            <ul className="sm:flex gap-3 hidden items-center sm:text-sm lg:text-lg">
+              <Link href="/" className="flex">
+                <span>Shop</span> <Image src={downArrow} alt="Arrow"></Image>
+              </Link>
+              <Link href="/">On Sale</Link>
+              <Link href="/arrivals">New Arrivals</Link>
+              <Link href="/">Brands</Link>
+            </ul>
+          </div>
+
+          {/* Search Div */}
+          <div className="flex w-[45%] relative items-center">
+            <Image
+              src={search}
+              alt="Search Icon"
+              className="absolute left-3 cursor-pointer hover:scale-105 sm:block hidden"
             />
             <input
               type="text"
-              placeholder="Search"
-              className="bg-[#efeeee] p-2 pl-10 rounded-full outline-none w-full md:w-auto"
+              className="bg-[#F0F0F0] w-full py-2 rounded-full px-12 sm:block hidden outline-none"
+              placeholder="Search for Products"
             />
           </div>
 
-          {/* Icons */}
+          {/* Cart Div */}
           <div className="flex gap-3">
-            <Link href={"/"}>
-              <Image src={"/heart.png"} height={24} width={24} alt="Heart" />
-            </Link>
-            <Link href={"/"}>
-              <Image src={"/bag.png"} height={24} width={24} alt="Bag" />
-            </Link>
+            <Image src={search1} alt="Search Icon" className="cursor-pointer hover:scale-105"></Image>
+            <Image src={cart} alt="Cart"></Image>
+            <Image src={account} alt="Account"></Image>
           </div>
         </div>
       </div>
-
-      {/* Mobile Navigation */}
-      <div className="block md:hidden bg-[#f5f5f5] px-4 py-2">
-        <ul className="flex flex-col gap-2 text-sm">
-          <Link href={"/"} className="block">New & Featured</Link>
-          <Link href={"/"} className="block">Men</Link>
-          <Link href={"/"} className="block">Women</Link>
-          <Link href={"/"} className="block">Kids</Link>
-          <Link href={"/"} className="block">Sales</Link>
-          <Link href={"/"} className="block">SNKRS</Link>
-        </ul>
-      </div>
-    </header>
+    </div>
   );
 }
